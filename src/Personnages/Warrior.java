2871f9bbcc991ccaa658276d737535f3c2c5af3e
FloSouchiere <59911947+FloSouchiere@users.noreply.github.com>
2020-04-28 16:21:18 +0200

    Add files via upload

---
+++ src/Personnages/Warrior.java
@@ -0,0 +1,28 @@
+package Personnages;
+
+import java.util.Scanner;
+
+public class Warrior {
+
+	// Attributes
+
+	private String name;
+	// private Image image;
+	private int life;
+	private int strength;
+	private String weapon;
+	private String shield;
+
+	// Constructor
+
+	public Warrior(String name, int life, int strength, String weapon, String shield) {
+
+		this.name = "test";
+		this.life = 10;
+		this.strength = 8;
+		this.weapon = "sword";
+		this.shield = "brooke";
+
+	}
+
+}
---
+++ src/Personnages/Wizard.java
@@ -0,0 +1,26 @@
+package Personnages;
+
+public class Wizard {
+
+	// Attributes
+
+	private String name;
+	// private Image image;
+	private int life;
+	private int strength;
+	private String weapon;
+	private String philter;
+
+	// Constructor
+
+	public Wizard(String name, int life, int strength, String weapon, String philter) {
+
+		this.name = "pinball";
+		this.life = 8;
+		this.strength = 10;
+		this.weapon = "stick";
+		this.philter = "syphon";
+
+	}
+
+}
